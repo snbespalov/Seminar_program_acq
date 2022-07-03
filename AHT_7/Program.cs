6f78953cdced9cebd80b905c6e9896ac3ff6d36f
BSN <snbespalov@gmail.com>
2022-07-03 23:20:52 +0200

    Completed Task 7 of additional tasks

---
+++ AHT_7/Program.cs
@@ -0,0 +1,8 @@
+﻿string number;
+
+Console.WriteLine("Добрый день! Введите трёхзначное число, и я вам назову последнюю цифру");
+number = Console.ReadLine();
+
+Console.WriteLine("Последняя цифра: ");
+Console.WriteLine(number[2]);
+
