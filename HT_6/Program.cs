55faa0dd9f3c5b27bf0f2eed8cee8fedd3a3bf82
BSN <snbespalov@gmail.com>
2022-07-03 18:25:29 +0200

    Complete Task 6 of the Hometasks

---
+++ HT_6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Добрый день! Я подскажу является ли ваше число четным. Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0 )
+    {
+    Console.WriteLine("Чётное");
+    }
+else
+    {
+    Console.WriteLine("Нечётное");
+    }
