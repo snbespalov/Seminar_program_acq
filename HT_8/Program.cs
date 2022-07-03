975cc841c8347aaa0d6694d50bc58694be601631
BSN <snbespalov@gmail.com>
2022-07-03 18:48:45 +0200

    Completed Task 8 of the Hometasks

---
+++ HT_8/Program.cs
@@ -0,0 +1,10 @@
+﻿int count, x;
+
+Console.WriteLine("Добрый день! Я покажу вам все чётные числа до числа, которое вы введете. Введите число");
+count = Convert.ToInt32(Console.ReadLine());
+
+for(x = 0; x<count; x++)
+    { 
+    if ( x % 2 == 0)
+    Console.WriteLine(x);
+    }
