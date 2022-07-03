3ba759be90ebb19bcc4c5f0e1cba6695bbf2d8d4
BSN <snbespalov@gmail.com>
2022-07-03 22:52:42 +0200

    Completed Task 5 of additional tasks

---
+++ AHT_5/Program.cs
@@ -0,0 +1,11 @@
+﻿int count, x;
+
+Console.WriteLine("День добрый! Я покажу вам все целые числа в промежутке от -N до N, где N - ваше число.");
+Console.WriteLine("Введите число");
+count = Convert.ToInt32(Console.ReadLine());
+
+for(x = -count; x <= count; x++)
+{
+    if(x % 1 == 0);
+    Console.WriteLine(x);
+}
