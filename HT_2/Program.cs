e4b2521f30d0bc0fc6d4c4afb43525f1f6c34d09
BSN <snbespalov@gmail.com>
2022-07-03 17:12:01 +0200

    Complete Task 2 from the Hometask

---
+++ HT_2/Program.cs
@@ -0,0 +1,21 @@
+﻿int minNumber;
+int maxNumber;
+
+Console.WriteLine("Добрый день! Я подскажу какое из чисел большее, а какое меньшее. Введите первое число");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+if (firstNumber < secondNumber)
+    {
+    minNumber = firstNumber;
+    maxNumber = secondNumber;
+    }
+    else
+    {
+    minNumber = secondNumber;
+    maxNumber = firstNumber;
+    }
+Console.WriteLine("Минимальное число: ");
+Console.WriteLine(minNumber);
+Console.WriteLine("Максимальное число: ");
+Console.WriteLine(maxNumber);
