eed99264ef02b27c70bda2934cf906b86f780fac
BSN <snbespalov@gmail.com>
2022-07-09 21:27:15 +0200

    Completed Seminar Task №11

---
+++ ST_11/Program.cs
@@ -0,0 +1,13 @@
+﻿// 11. Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int rndNum = new Random().Next(100,1000);
+Console.WriteLine($"Случайное трёхзначное число {rndNum}");
+
+int firstDigit = rndNum / 100;
+int secondDigit = rndNum % 10;
+Console.WriteLine($"Результат удаления цифры {firstDigit}{secondDigit}");
