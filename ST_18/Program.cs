10b8c087fe0727b1d7ae1e68ad36829896b3a01d
BSN <snbespalov@gmail.com>
2022-07-11 00:11:09 +0200

    Added additional solvation to Seminar TA18

--- ST_18/Program.cs
+++ ST_18/Program.cs
@@ -1,19 +1,30 @@
 ﻿// 18. Напишите программу, которая по заданному номеру чевтерти,
 // показывает диапазон возможных координат точек X и Y.
 
+// Решение 1
 Console.Clear();
 Console.WriteLine("Введите номер четверти");
 int x = Convert.ToInt32(Console.ReadLine());
 
+// string Coordiantes(int xc)
+// {
+//     if (xc == 1) return "X > 0, Y > 0";
+//     if (xc == 2) return "X < 0, Y > 0";
+//     if (xc == 3) return "X < 0, Y < 0";
+//     if (xc == 4) return "X > 0, Y < 0";
+//     return "Введен некорректный номер";
+// }
 
-string Coordiantes(int xc)
+// string result = Coordiantes(x);
+// Console.WriteLine(result);
+
+// Решение 2
+
+switch(x)
 {
-    if (xc == 1) return "X > 0, Y > 0";
-    if (xc == 2) return "X < 0, Y > 0";
-    if (xc == 3) return "X < 0, Y < 0";
-    if (xc == 4) return "X > 0, Y < 0";
-    return "Введен некорректный номер";
+    case 1: Console.WriteLine("X > 0, Y > 0"); break;
+    case 2: Console.WriteLine("X < 0, Y > 0"); break;
+    case 3: Console.WriteLine("X < 0, Y < 0"); break;
+    case 4: Console.WriteLine("X > 0, Y < 0"); break;
+    default: Console.WriteLine("Введен некорректный номер"); break;
 }
-
-string result = Coordiantes(x);
-Console.WriteLine(result);
