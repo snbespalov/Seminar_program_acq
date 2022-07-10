26840986a9cb36683f63fc5511b43ea9ad9a4183
BSN <snbespalov@gmail.com>
2022-07-10 15:55:20 +0200

    Another way to solve Seminar Task №14

--- ST_14/Program.cs
+++ ST_14/Program.cs
@@ -5,24 +5,37 @@
 // 46 -> нет
 // 161 -> да
 
-Console.WriteLine("Введите число");
-int userNum = Convert.ToInt32(Console.ReadLine());
-int res7;
-int res23;
+// Решение 1
+// Console.WriteLine("Введите число");
+// int userNum = Convert.ToInt32(Console.ReadLine());
+// int res7;
+// int res23;
 
-int result7 (int num1)
+// int result7 (int num1)
+// {
+//     int num = userNum;
+//     if (num % 7 == 0) res7 = 0;
+//     else res7 = 1;
+//     return res7;
+// }
+// int result23 (int num2)
+// {
+//     int num = userNum;
+//     if (num % 23 == 0) res23 = 0;
+//     else res23 = 2;
+//     return res23;
+// }
+// string res = result7 (userNum) == result23 (userNum) ? "Число кратно 7 и 23" : "Число не кратно 7 и 23";
+// Console.WriteLine(res);
+
+// Решение 2
+
+ Console.WriteLine("Введите число");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+if(userNum % 7 == 0 && userNum % 23 == 0)
 {
-    int num = userNum;
-    if (num % 7 == 0) res7 = 0;
-    else res7 = 1;
-    return res7;
+Console.WriteLine("Число кратно 7 и 23");
 }
-int result23 (int num2)
-{
-    int num = userNum;
-    if (num % 23 == 0) res23 = 0;
-    else res23 = 2;
-    return res23;
-}
-string res = result7 (userNum) == result23 (userNum) ? "Число кратно 7 и 23" : "Число не кратно 7 и 23";
-Console.WriteLine(res);
+else
+Console.WriteLine("Число не кратно 7 и 23");
