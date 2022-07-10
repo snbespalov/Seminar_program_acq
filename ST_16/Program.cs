76db0a82273026c2964e945e4aa3d735fffddba4
BSN <snbespalov@gmail.com>
2022-07-10 16:16:54 +0200

    Another way to solve Seminar Task №16

--- ST_16/Program.cs
+++ ST_16/Program.cs
@@ -6,11 +6,23 @@
 // 25, 5 -> да
 // 8,9 -> нет
 
+
+// Решение 1
+//Console.WriteLine("Введите первое число");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num2 * num2 == num1) Console.WriteLine($"Первое число {num1} является квадратом второго числа {num2}");
+// else if (num1 * num1 == num2) Console.WriteLine($"Второе число {num2} является квадратом первого числа {num1}");
+// else Console.WriteLine("Ни одно из чисел не является квадратом другого.");
+
+// Решение 2ё
+Console.WriteLine("Я проверю является ли одно число квадратом второго числа");
 Console.WriteLine("Введите первое число");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
-if (num2 * num2 == num1) Console.WriteLine($"Первое число {num1} является квадратом второго числа {num2}");
-else if (num1 * num1 == num2) Console.WriteLine($"Второе число {num2} является квадратом первого числа {num1}");
-else Console.WriteLine("Ни одно из чисел не является квадратом другого.");
+if (num2 * num2 == num1 || num1 * num1 == num2) Console.WriteLine("Да, является");
+else  Console.WriteLine("Нет, не является");
